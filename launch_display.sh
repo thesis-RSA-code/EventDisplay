eb92ccf82bbdefef5e42d2f819be437f44f01d97
Erwanlbv <erwan.le-blevec@polytechnique.edu>
2024-11-13 04:32:23 +0100

    remove tk option, using event_display context only

--- launch_display.sh
+++ launch_display.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
 file_path=/sps/t2k/eleblevec/WCSimPackage/GraphDatasets/RootExplorer/outputs/test_c_v1.root
-events='8637:8640' # Attention à bien faire event + 1
-tree_name='filtered_tree'
+events='0:1900'
+tree_name=filtered_tree
+
+sp=/sps/t2k/eleblevec/BigBrother/EventDisplay/outputs/
+sf=one_display_test.png
 
 python event_display.py \
     -e 'HK' \
     -t $tree_name \
     -f $file_path \
-    -d $events
+    -d $events \
+    -sp $sp \
